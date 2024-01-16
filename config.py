84e730514e58a03c9e78f9b9688ef63eb5e06285
vrublych <vrublych34@gmail.com>
2024-01-16 16:54:40 +0300

    HW_2

---
+++ config.py
@@ -0,0 +1,5 @@
+import os.path
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+YT_CHANNEL_DATA = os.path.join(ROOT_DIR, 'homework-2', 'moscowpython.json')
--- src/channel.py
+++ src/channel.py
@@ -1,53 +1,55 @@
-import os
 import json
+import os
 from googleapiclient.discovery import build
-import isodate
+from config import YT_CHANNEL_DATA
+
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key: str = os.getenv('YOUTUBE_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        api_key: str = os.getenv('YOUTUBE_API_KEY')
-        self.channel_id = channel_id
-        self.youtube = build('youtube', 'v3', developerKey=api_key)
-        self.refresh_data()
-
-    def refresh_data(self) -> None:
-        """Обновляет данные о канале."""
-        response = self.youtube.channels().list(
-            id=self.channel_id,
-            part='snippet,statistics'
-        ).execute()
-        channel_data = response.get('items', [])[0]
-
-        self.title = channel_data['snippet']['title']
-        self.description = channel_data['snippet']['description']
-        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
-        self.subscriber_count = int(channel_data['statistics']['subscriberCount'])
-        self.video_count = int(channel_data['statistics']['videoCount'])
-        self.view_count = int(channel_data['statistics']['viewCount'])
-
-    @classmethod
-    def get_service(cls):
-        """Возвращает объект для работы с YouTube API."""
-        api_key: str = os.getenv('YOUTUBE_API_KEY')
-        return build('youtube', 'v3', developerKey=api_key)
-
-    def to_json(self, filename: str) -> None:
-        """Сохраняет значения атрибутов экземпляра в файл."""
-        data = {
-            'id': self.channel_id,
+        self.__channel_id = channel_id
+        self.channel_data = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.channel_data['items'][0]['snippet']['title']
+        self.description = self.channel_data['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.subscriber_count = self.channel_data['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel_data['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel_data['items'][0]['statistics']['subscriberCount']
+        self.new_data = {
+            'id': self.__channel_id,
             'title': self.title,
             'description': self.description,
             'url': self.url,
-            'subscriber_count': self.subscriber_count,
+            'subs_count': self.subscriber_count,
             'video_count': self.video_count,
-            'view_count': self.view_count
+            'view_count': self.view_count,
         }
 
-        with open(filename, 'w', encoding='utf-8') as json_file:
-            json.dump(data, json_file, ensure_ascii=False, indent=2)
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, new_channel_id):
+        if new_channel_id:
+            self.__channel_id = new_channel_id
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YOUTUBE_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, file_name):
+        json_data = json.dumps(self.new_data, ensure_ascii=False)
+
+        with open(f"../homework-2/{file_name}", 'w', encoding='windows-1251') as file:
+            file.write(json_data)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -55,4 +57,4 @@ class Channel:
             id=self.channel_id,
             part='snippet,statistics'
         ).execute()
-        print(json.dumps(response, indent=2, ensure_ascii=False))
+        print(response)
