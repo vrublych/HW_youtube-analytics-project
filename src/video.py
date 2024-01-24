38429177648d24cf81f0cdf44679b05ade027e33
vrublych <vrublych34@gmail.com>
2024-01-24 14:55:14 +0300

    HW_4: add video.py and class

---
+++ src/video.py
@@ -0,0 +1,32 @@
+import os
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv('YOUTUBE_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+class Video:
+    def __init__(self, video_id):
+        self.__video_id = video_id
+        self.video_data = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id).execute()
+        self.video_title = self.video_data['items'][0]['snippet']['title']
+        self.video_url = f'https://www.youtube.com/watch?v={self.__video_id}'
+        self.views = self.video_data['items'][0]['statistics']['viewCount']
+        self.likes = self.video_data['items'][0]['statistics']['likeCount']
+
+    @property
+    def video_id(self):
+        return self.__video_id
+
+    def __str__(self):
+        return f"{self.video_title}"
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.__playlist_id = playlist_id
+
+    @property
+    def playlist_id(self):
+        return self.__playlist_id
